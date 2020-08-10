cc8899fbaae36445a70a9561b50769680dce9c42
Jsquad <lakis006@umn.edu>
2020-08-10 02:43:50 -0500

    testing out the app real-time and offline

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 2500;
+const PORT = 3500;
 
 const app = express();
 
