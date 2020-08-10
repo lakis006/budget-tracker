a0896350f5533bc15644ff31bf4a8496fd85caa2
Jsquad <lakis006@umn.edu>
2020-08-10 03:09:25 -0500

    started adding event listeners to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,5 +3,14 @@ console.log("Hello from your service worker!")
 const FILES_TO_CACHE = [
     'icons\icon-192x192.png',
     'icons\icon-512x512.png',
-    
-]
+    'indexedDB.js',
+    'index.html',
+    '/',
+    'index.js',
+    'service=worker.js',
+    'styles.css'    
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
