cd81b3b2a8aaf192f3d6bc9697fc0e6db6bb48af
Jsquad <lakis006@umn.edu>
2020-08-10 04:06:14 -0500

    finished creating anew databse request & connection, inserted onupgradedneeded function to create a new object store named pending, save result, added record and lastly be able to post to our mongodb database

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,40 +1,59 @@
-export function useIndexedDb(databaseName, storeName, method, object) {
-    return new Promise((resolve, reject) => {
-      const request = window.indexedDB.open(databaseName, 1);
-      let db,
-        tx,
-        store;
-  
-      request.onupgradeneeded = function(e) {
-        const db = request.result;
-        db.createObjectStore(storeName, { keyPath: "_id" });
-      };
-  
-      request.onerror = function(e) {
-        console.log("There was an error");
-      };
-  
-      request.onsuccess = function(e) {
-        db = request.result;
-        tx = db.transaction(storeName, "readwrite");
-        store = tx.objectStore(storeName);
-  
-        db.onerror = function(e) {
-          console.log("error");
-        };
-        if (method === "put") {
-          store.put(object);
-        }
-        if (method === "get") {
-          const all = store.getAll();
-          all.onsuccess = function() {
-            resolve(all.result);
-          };
-        }
-        tx.oncomplete = function() {
-          db.close();
-        };
-      };
-    });
+
+let db
+const request = window.indexedDB.open("budget", 1);
+
+
+
+request.onupgradeneeded = function (e) {
+  const db = event.target.result;
+  let storedObject = db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
   }
-  
+};
+
+
+request.onerror = function (event) {
+  console.log("Looks like there was an error", event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const pendingStore = transaction.storedObject("pending");
+
+  pendingStore.add(record); //adding a record 
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const pendingStore = transaction.storedObject("pending");
+  const records = pendingStore.getAll();
+
+
+  records.onsuccess = function () {
+    if (records.result.length > 0) {
+      fetch("/api/transition/bulk", {
+        method: "POST",
+        body: JSON.stringify(records.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const pendingStore = transaction.storedObject("pending");
+
+          pendingStore.clear();
+        });
+    }
+  }
+}
+
+window.addEventListener("online", checkDatabase); //app listener 
